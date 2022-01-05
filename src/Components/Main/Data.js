2b66a6bf0a46be245a42d1eb4c4bf6459faa91fa
kshieldsDTS <escanor05@gmail.com>
2022-01-05 11:35:11 -0500

    create context to store user and enemy data

---
+++ src/Components/Main/Data.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export const PlayerData = React.createContext(null);
+export const EnemyData = React.createContext(null)
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import Status from './Status/Status'
-import Inventory from './Inventory/Inventory'
-import Narrative from './Narrative/Narrative'
+import { useState } from 'react';
+import { PlayerData } from './Data';
+import { EnemyData } from './Data';
+import Status from './Status/Status';
+import Inventory from './Inventory/Inventory';
+import Narrative from './Narrative/Narrative';
 
 function Main(props) {
+    const [playerData, setPlayerData] = useState({
+        hp: 50,
+        ac: 16,
+        attackBonus: 5,
+        inventory: [{
+            weapon: true,
+            armor: true,
+            shield: true,
+        }
+        ]
+    })
+    const [enemyData, setEnemyData] = useState({})
     return (
         <div>
             Hello from Main
-            <Status />
-            <Inventory />
-            <Narrative />
+            <PlayerData.Provider value={{ playerData, setPlayerData }}>
+                <Status />
+                <Inventory />
+            <EnemyData.Provider value={{enemyData, setEnemyData }}>
+                <Narrative />
+            </EnemyData.Provider>
+            </PlayerData.Provider>         
         </div>
     );
 }
