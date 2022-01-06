62281b30fbcabe264fb6d083069fd64ab47a5a93
kshieldsDTS <escanor05@gmail.com>
2022-01-06 16:33:34 -0500

    integrate api fetch and use parseInt() and slice() black magic to turn string data into usable numbers for calculations

--- src/Components/Main/Narrative/Narrative.js
+++ src/Components/Main/Narrative/Narrative.js
@@ -13,30 +13,25 @@ function Narrative(props) {
         enemyWeapon: 'axe'
     }
     const [enemyStats, setEnemyStats] = useState(orc)
-    function handleTest(){
-        console.log(props.story[props.currentTextID].options[0].text);
-        props.setCurrentTextID(1)
-    };
     return (
         <div className='narrative-container'>
             <div className='narrative'>
                 <p>{props.story[props.currentTextID].text}</p>
-                <button onClick={handleTest}>Test</button>
                 {props.story[props.currentTextID].options.map((e, index) => {
                     return (
-											<Options
-												key={`${props.story[props.currentTextID].id}${props.story[props.currentTextID].options[index].text}`}
-                                                optionsText={props.story[props.currentTextID].options[index].text}
-                                                nextID={props.story[props.currentTextID].options[index].nextID}
-												text={props.story[props.currentTextID]}
-												playerStats={props.playerStats}
-												enemyStats={enemyStats}
-												setPlayerStats={props.setPlayerStats}
-												setEnemyStats={setEnemyStats}
-                                                setCurrentTextID={props.setCurrentTextID}
-											/>
-										);
-                })}
+						<Options
+							key={`${props.story[props.currentTextID].id}${props.story[props.currentTextID].options[index].text}`}
+                            optionsText={props.story[props.currentTextID].options[index].text}
+                            nextID={props.story[props.currentTextID].options[index].nextID}
+							text={props.story[props.currentTextID]}
+							playerStats={props.playerStats}
+							enemyStats={enemyStats}
+							setPlayerStats={props.setPlayerStats}
+							setEnemyStats={setEnemyStats}
+                            setCurrentTextID={props.setCurrentTextID}
+						/>
+					);
+                })}<p>Orc HP: {enemyStats.enemyCurrentHP}/{enemyStats.enemyMaxHP}</p>
             </div>
         </div>
     );
