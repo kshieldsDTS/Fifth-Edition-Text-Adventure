f9d303392c8fe882c3c44be5d47597600fcd57ad
kshieldsDTS <escanor05@gmail.com>
2022-01-07 10:19:36 -0500

    build out major portions for combat modal

--- src/Components/Main/Narrative/CombatModal/CombatModal.js
+++ src/Components/Main/Narrative/CombatModal/CombatModal.js
@@ -1,15 +1,59 @@
 import React from 'react';
+import { useState } from 'react'
 
 function CombatModal(props) {
+    const [playerTurn, setPlayerTurn] = useState(true)
     function closeModal() {
         props.setEnemyStats({})
     }
+    function playerAttack() {
+            const damage = Math.floor(Math.random()*props.playerStats.items.weapon.maxDamage +props.playerStats.items.weapon.minDamage)*2+props.playerStats.damageBonus;
+			if (Math.floor(Math.random()*20+1)===20) {
+				props.setEnemyStats({ ...props.enemyStats, currentHP: props.enemyStats.currentHP - damage*2 });
+				console.log('playerCrit');	
+			} else if (Math.floor(Math.random()*20+1+props.playerStats.attackBonus)>=props.enemyStats.armorClass) {			
+				props.setEnemyStats({...props.enemyStats,currentHP: props.enemyStats.currentHP-damage,});	
+				console.log('player hit');
+			} else if (Math.floor(Math.random()*20+1+props.playerStats.attackBonus)<props.enemyStats.armorClass) {
+				console.log('player miss');
+			} else if (Math.floor(Math.random() * 20 + 1) === 1) {	
+			    console.log('player crit miss');
+			}
+		}			
+    function enemyAttack() {
+        const damage = Math.floor(Math.random()*props.enemyStats.maxDamage+props.enemyStats.minDamage)+props.enemyStats.damageBonus;
+			if (Math.floor(Math.random()*20+1)===20) {		
+				props.setPlayerStats({ ...props.playerStats,currentHP: props.playerStats.currentHP-damage*2 });
+				console.log('orc crit');
+			} else if (Math.floor(Math.random()*20+1+props.enemyStats.attackBonus)>=props.playerStats.armorClass) {
+			    props.setPlayerStats({ ...props.playerStats, currentHP: props.playerStats.currentHP - damage });
+                console.log('orc hit');
+			} else if (
+				Math.floor(Math.random() * 20 + 1 + props.enemyStats.attackBonus) <props.playerStats.armorClass) {
+				console.log('orc miss');
+			} else if (Math.floor(Math.random() * 20 + 1) === 1) {
+				console.log('orc crit miss');
+			}
+		}
     return (
-        <div className='combat-modal'>
-            Combat Modal Test 1
-            <button onClick={closeModal}>Close Modal</button>
-        </div>
-    );
+			<div className='combat-modal'>
+				<p>{props.enemyStats.nameUpper}</p>
+				<p>HP: {props.enemyStats.currentHP}/{props.enemyStats.maxHP}</p>
+				<p>Armor Class: {props.enemyStats.armorClass}</p>
+				<p>Attack Bonus: {props.enemyStats.attackBonus}</p>
+				<p>Minimum Damage: {props.enemyStats.minDamage}</p>
+				<p>Maximum Damage: {props.enemyStats.maxDamage}</p>
+				<p>Bonus Damage: {props.enemyStats.damageBonus}</p>
+				<button onClick={closeModal}>Close Modal</button>
+                {playerTurn ? 
+                    <div>
+                        <button onClick={playerAttack}>Attack the {props.enemyStats.lowerName}</button>
+                        <button>Flee!</button>
+                    </div> :
+                    <button onClick={enemyAttack}>Prepare for the {props.enemyStats.lowerName}'s attack!</button>
+                }
+			</div>
+		);
 }
 
 export default CombatModal;
--- src/Components/Main/Narrative/Narrative.js
+++ src/Components/Main/Narrative/Narrative.js
@@ -4,108 +4,59 @@ import { useState } from 'react'
 import CombatModal from './CombatModal/CombatModal';
 
 function Narrative(props) {
-        const [enemyStats, setEnemyStats] = useState({});
-				function playerAttack() {
-                    if (Math.floor(Math.random()*20+1)===20) {
-                        const damage = Math.floor(Math.random()*props.playerStats.items.weapon.maxDamage+props.playerStats.items.weapon.minDamage)*2+props.playerStats.damageBonus;
-                        setEnemyStats({...enemyStats, currentHP: enemyStats.currentHP-damage })
-						console.log('playerCrit');
-					} else if (Math.floor(Math.random()*20+1+props.playerStats.attackBonus)>=enemyStats.armorClass) {
-                        const damage = Math.floor(Math.random()*props.playerStats.items.weapon.maxDamage+props.playerStats.items.weapon.minDamage)+props.playerStats.damageBonus;	
-                        setEnemyStats({...enemyStats, currentHP: enemyStats.currentHP-damage })
-						console.log('player hit');
-					} else if (Math.floor(Math.random()*20+1+props.playerStats.attackBonus)<enemyStats.armorClass) {
-							console.log('player miss');
-					} else if (Math.floor(Math.random() * 20 + 1) === 1) {
-							console.log('player crit miss');
-					}					    
-				}
-				function enemyAttack() {
-                    if (Math.floor(Math.random()*20+1) === 20) {
-                        const damage = Math.floor(Math.random()*enemyStats.maxDamage+enemyStats.minDamage)*2+enemyStats.damageBonus
-                        props.setPlayerStats ({...props.playerStats, currentHP: props.playerStats.currentHP-damage })
-						console.log('orc crit');
-					} else if (Math.floor(Math.random()*20+1+enemyStats.attackBonus)>=props.playerStats.armorClass) {
-                        const damage = Math.floor(Math.random()*enemyStats.maxDamage +enemyStats.minDamage)+enemyStats.damageBonus	
-                        props.setPlayerStats ({...props.playerStats, currentHP: props.playerStats.currentHP-damage })
-						console.log('orc hit');
-					} else if (Math.floor(Math.random()*20+1+enemyStats.attackBonus)<props.playerStats.playerArmorClass) {
-							console.log('orc miss');
-					} else if (Math.floor(Math.random() * 20 + 1) === 1) {
-							console.log('orc crit miss');
-					}					    
-				}
-				function handleCombat(){
-					if (enemyStats.currentHP > 0) {
-						playerAttack()
-					} else {
-						console.log('the enemy is slain');
-					}
-					if (props.playerStats.playerCurrentHP > 0) {
-						enemyAttack()
-					} else {
-						console.log('you are slain');
-					}
-					
-				}
-				function fetchEnemy() {
-					const url = 'https://www.dnd5eapi.co/api/monsters/orc';
-					fetch(url)
-						.then((res) => res.json())
-						.then((res) => {
-							const damageString = res.actions[0].damage[0].damage_dice;
-							const numberOfDice = parseInt(damageString.slice(0, 1));
-							const diceDamage = parseInt(damageString.slice(2, 4));
-							setEnemyStats({
-								nameLower: res.index,
-								nameUpper: res.name,
-								maxHP: parseInt(res.hit_points),
-								currentHP: parseInt(res.hit_points),
-								armorClass: parseInt(res.armor_class),
-								minDamage: parseInt(numberOfDice),
-								maxDamage: parseInt(numberOfDice * diceDamage),
-								attackBonus: parseInt(res.actions[0].attack_bonus),
-								damageBonus: parseInt(damageString.slice(-1)),
-								active: true
-							});
-						});
-				}
+    const [enemyStats, setEnemyStats] = useState({});
+	function fetchEnemy() {
+		const url = 'https://www.dnd5eapi.co/api/monsters/orc';
+		fetch(url)
+			.then((res) => res.json())
+			.then((res) => {
+				const damageString = res.actions[0].damage[0].damage_dice;
+				const numberOfDice = parseInt(damageString.slice(0, 1));
+				const diceDamage = parseInt(damageString.slice(2, 4));
+				setEnemyStats({
+					nameLower: res.index,
+					nameUpper: res.name,
+					maxHP: parseInt(res.hit_points),
+					currentHP: parseInt(res.hit_points),
+					armorClass: parseInt(res.armor_class),
+					minDamage: parseInt(numberOfDice),
+					maxDamage: parseInt(numberOfDice * diceDamage),
+					attackBonus: parseInt(res.actions[0].attack_bonus),
+					damageBonus: parseInt(damageString.slice(-1)),
+					active: true
+				});
+			});
+	}
     return (
-			<div className='narrative-container'>
-				<div className='narrative'>
-					<p>{enemyStats.nameUpper}</p>
-                    <p>HP: {enemyStats.currentHP}/{enemyStats.maxHP}</p>
-					<p>Armor Class: {enemyStats.armorClass}</p>
-					<p>Attack Bonus: {enemyStats.attackBonus}</p>
-					<p>Minimum Damage: {enemyStats.minDamage}</p>
-					<p>Maximum Damage: {enemyStats.maxDamage}</p>
-					<p>Bonus Damage: {enemyStats.damageBonus}</p>
-					<button onClick={fetchEnemy}>Fetch Orc</button>
-					<button onClick={handleCombat}>Fight Orc</button>
-					{enemyStats.active ? 
-						<CombatModal 
-							enemyStats={enemyStats}
+		<div className='narrative-container'>
+			<div className='narrative'>
+				<button onClick={fetchEnemy}>Fetch Orc</button>
+				{enemyStats.active ? 
+					<CombatModal 
+						enemyStats={enemyStats}
+						playerStats={props.playerStats}
+						setEnemyStats={setEnemyStats}
+						setPlayerStats={props.setPlayerStats}
+					/> : null}
+				{enemyStats.active ? null : 
+					<p>{props.story[props.currentTextID].text}</p>}
+				{enemyStats.active ? null :
+				props.story[props.currentTextID].options.map((e, index) => {
+					return (
+						<Options
+							key={`${props.story[props.currentTextID].id}${props.story[props.currentTextID].options[index].text}`}
+							optionsText={props.story[props.currentTextID].options[index].text}
+							nextID={props.story[props.currentTextID].options[index].nextID}
+							text={props.story[props.currentTextID]}
 							playerStats={props.playerStats}
-							setEnemyStats={setEnemyStats}
 							setPlayerStats={props.setPlayerStats}
-						/> : null}
-					<p>{props.story[props.currentTextID].text}</p>
-					{props.story[props.currentTextID].options.map((e, index) => {
-						return (
-							<Options
-								key={`${props.story[props.currentTextID].id}${props.story[props.currentTextID].options[index].text}`}
-								optionsText={props.story[props.currentTextID].options[index].text}
-								nextID={props.story[props.currentTextID].options[index].nextID}
-								text={props.story[props.currentTextID]}
-								playerStats={props.playerStats}
-								setPlayerStats={props.setPlayerStats}
-								setCurrentTextID={props.setCurrentTextID}
-							/>
-						);
-					})}
-				</div>
+							setCurrentTextID={props.setCurrentTextID}
+						/>
+					);
+				})}
 			</div>
-		);
+		</div>
+	);
 }
 
 export default Narrative;
