fc4beba6e5b24c34cd699cc558e4fb77d25dbb9b
kshieldsDTS <escanor05@gmail.com>
2022-01-10 10:35:38 -0500

    change styling to fix rendering issues

--- src/Components/Main/Status/Status.js
+++ src/Components/Main/Status/Status.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Status(props) {
     return (
         <div className='character-status'>
-            <img src='https://www.scabard.com/user/Grimmtales/image/armor_plate_mail.png' alt='Player Icon' className='icon'></img>
             <div className='hp'>
                 <p>HP: {props.currentHP}/{props.maxHP}</p>
                 <progress id='health' value={props.currentHP} max={props.maxHP}/>
