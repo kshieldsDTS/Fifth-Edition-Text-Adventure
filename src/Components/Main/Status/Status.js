f9b2e97c7f180097e155efc0981dd21984e63b0f
kshieldsDTS <escanor05@gmail.com>
2022-03-23 17:46:20 -0400

    begin status component styling, create message to show app is under construction

--- src/Components/Main/Status/Status.js
+++ src/Components/Main/Status/Status.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 function Status(props) {
     return (
-        <div className='character-status'>
-            <div className='hp'>
-                <p>HP: {props.currentHP}/{props.maxHP}</p>
-                <progress id='health' value={props.currentHP} max={props.maxHP}/>
-            </div>
-            <div className='armor'>
-                <p>Armor Class: {props.armorClass}</p>
-            </div>
-            <div className='atb'>
-                <p>Attack Bonus: {props.attackBonus}</p>
-            </div>
-        </div>
-    );
+		<div className='character-status'>
+            <h1>Currently undergoing maintenance!</h1>
+			<div className='hp'>
+				<p>
+					HP: {props.currentHP}/{props.maxHP}
+				</p>
+				<progress id='health' value={props.currentHP} max={props.maxHP} />
+			</div>
+			<div className='bonuses'>
+				<p>Armor Class: {props.armorClass}</p>
+				<p>Attack Bonus: {props.attackBonus}</p>
+			</div>
+		</div>
+	);
 }
 
 export default Status;
